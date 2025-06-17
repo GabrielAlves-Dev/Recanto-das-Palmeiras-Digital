6133ca3b6eea807a345385ed6c953e230e4e73e3
GabrielAlves-Dev <pessoa80gabriel@gmail.com>
2025-06-17 08:26:11 -0300

    expections

---
+++ backend/src/main/java/com/github/GabrielAlves_dev/recanto_das_palmeiras_digital/Exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.github.GabrielAlves_dev.recanto_das_palmeiras_digital.Exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/github/GabrielAlves_dev/recanto_das_palmeiras_digital/Exceptions/RestExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.github.GabrielAlves_dev.recanto_das_palmeiras_digital.Exceptions;
+
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInternalServerError(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Erro interno do servidor", List.of("Ocorreu um erro inesperado. Tente novamente mais tarde.")));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Argumento Invalido",
+                List.of(ex.getMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(ChangeSetPersister.NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("Recurso não encontrado", List.of(ex.getMessage())));
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(ValidationException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Erro de validação",
+                ex.getErrorMessages() != null ? ex.getErrorMessages() : List.of("Erro de validação desconhecido")
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+
+}

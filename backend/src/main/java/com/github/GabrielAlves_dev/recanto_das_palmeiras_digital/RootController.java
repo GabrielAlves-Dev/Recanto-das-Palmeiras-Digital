b59fab553c667f171bf134e14cb0392de38dbeb5
Tiago Tito <tiago.tito.sampaio@gmail.com>
2025-06-24 12:24:52 -0300

    adcionei endpoint root para teste

---
+++ backend/src/main/java/com/github/GabrielAlves_dev/recanto_das_palmeiras_digital/RootController.java
@@ -0,0 +1,14 @@
+package com.github.GabrielAlves_dev.recanto_das_palmeiras_digital;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("Recanto das Palmeiras Digital");
+    }
+}

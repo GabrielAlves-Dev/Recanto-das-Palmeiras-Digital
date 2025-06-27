92c8e572782905f7556df0e8c7fcb93e51cef096
Tiago Tito <tiago.tito@alu.ufc.br>
2025-06-26 21:45:19 -0300

    corrigi imagens

--- backend/src/main/java/com/github/GabrielAlves_dev/recanto_das_palmeiras_digital/configuration/WebConfig.java
+++ backend/src/main/java/com/github/GabrielAlves_dev/recanto_das_palmeiras_digital/configuration/WebConfig.java
@@ -1,3 +1,6 @@
+package com.github.GabrielAlves_dev.recanto_das_palmeiras_digital.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +10,13 @@ import java.nio.file.Paths;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${produto.imagem.diretorio}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path uploadPath = Paths.get(uploadDir).toAbsolutePath();
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations("file:" + uploadPath.toString() + "/");
     }
 }

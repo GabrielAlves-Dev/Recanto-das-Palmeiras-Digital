18f22aebca322e490409a8ec12f66f9b1e16df25
GabrielAlves-Dev <pessoa80gabriel@gmail.com>
2025-07-29 15:08:15 -0300

    ativar trigger + correcao cicd front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -31,7 +31,6 @@ export function App() {
           <Route path="/unauthorized" element={<Unauthorized />} />
           
           {/* Alteracao para ativar o trigger */}
-          {/* Alteracao para ativar o trigger */}
           
           <Route element={<Layout />}>
             
